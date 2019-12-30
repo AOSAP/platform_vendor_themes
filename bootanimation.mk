1015776c1dfb354415e30dbb5eb2bbf1a125f239
mracar07 <umutcan@umutcanacar.me>
2019-12-30 07:56:31 +0000

    Add AOSAP Bootanimation
    Thanks to EdoZullo on xda
    
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,17 +1,2 @@
 # Boot Animation
-scr_resolution := 1080x1920
-abc_device := $(patsubst %f,%,$(subst abc_,,$(TARGET_PRODUCT)))
-
-ifneq ($(filter taimen,$(abc_device)),)
-scr_resolution := 1440x2880
-endif
-
-ifneq ($(filter bonito,$(abc_device)),)
-scr_resolution := 1080x2160
-endif
-
-ifneq ($(wildcard vendor/themes/bootanimation/$(scr_resolution).zip),)
-PRODUCT_COPY_FILES += \
-    vendor/themes/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
-    vendor/themes/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
-endif
+PRODUCT_COPY_FILES += vendor/themes/bootanimation/bootanimation.zip:system/media/bootanimation.zip
